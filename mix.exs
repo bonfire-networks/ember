d59626be2e956a1d2c684eef5a4393582f78dfe6
Mayel de Borniol <git@mayel.space>
2025-02-14 10:17:54 +0000

    add deps

--- mix.exs
+++ mix.exs
@@ -45,8 +45,48 @@ defmodule Ember.MixProject do
 
           {:floki, ">= 0.0.0", only: [:dev, :test]},
 
-          {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
-        ])
+          {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+
+
+          {:finch, "~> 0.16"},
+          # {:tz, "~> 0.26.2"},
+          
+          # tests
+          {:mneme, ">= 0.0.0", only: [:dev, :test]},
+
+          # error reporting
+          {:sentry, "~> 10.0", optional: true},
+          {:orion, "~> 1.0.5"},
+          # {:live_admin, #"~> 0.12.0"
+          # git: "https://github.com/bonfire-networks/live_admin"
+          # },
+
+          # API
+          # {:exonerate, "~> 1.1.3", runtime: Mix.env() != :prod},
+          # {:yaml_elixir, "~> 2.9"},
+
+          ## dev conveniences
+          {:phoenix_live_reload, "~> 1.3", optional: true},
+          {:pbkdf2_elixir, "~> 2.0", optional: true}
+
+          ] ++
+
+          if(System.get_env("AS_DESKTOP_WEBAPP") in ["1", "true"], do: [
+            {:elixirkit, git: "https://github.com/livebook-dev/livebook", sparse: "elixirkit"}
+            ], else: []) ++
+
+          if(System.get_env("AS_DESKTOP_APP") in ["1", "true"], do: [
+                {:desktop, github: "elixir-desktop/desktop"}
+                ], else: []) ++
+
+          if(System.get_env("WITH_API_GRAPHQL")== "yes", do: [
+                {:absinthe, "~> 1.7"},
+                {:bonfire_api_graphql,
+                git: "https://github.com/bonfire-networks/bonfire_api_graphql"},
+                  {:absinthe_client,
+                git: "https://github.com/bonfire-networks/absinthe_client"}
+              ], else: []) 
+        )
     ]
   end
 
