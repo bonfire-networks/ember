adb9775329fedff63bcd977c241710df97074e3c
Mayel de Borniol <git@mayel.space>
2026-06-29 17:08:35 +0100

    Update bonfire_common.exs

--- config/bonfire_common.exs
+++ config/bonfire_common.exs
@@ -55,6 +55,8 @@ config :ex_cldr,
   default_backend: Bonfire.Common.Localise.Cldr,
   json_library: Jason
 
+config :mdex_native, syntax_highlighter: :lumis
+
 pg_username = System.get_env("POSTGRES_USER") || "postgres"
 pg_pw = System.get_env("POSTGRES_PASSWORD") || "postgres"
 database = System.get_env("POSTGRES_DB") || "bonfire_dev"
@@ -82,5 +84,6 @@ config :sql,
       ssl: false
     }
   ]
+
 
 config :rustler_precompiled, force_build_all: System.get_env("RUSTLER_BUILD_ALL") in ["true", "1"]
